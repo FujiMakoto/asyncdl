3e298d7fc1d34d7cc3b1ebf286c54ffaa8ae102b
Makoto <makoto+github@taiga.sh>
2020-12-17 03:18:27 -0500

    Initial release

--- asyncdl/asyncdl.py
+++ asyncdl/asyncdl.py
@@ -1,44 +1,52 @@
 import asyncio
+import io
+import ipaddress
+import os
 import socket
+import typing
 from urllib.parse import urlparse
 
-from aiodns.error import DNSError
-from aiohttp import ClientSession
 from aiodns import DNSResolver
-from typing import *
-
+from aiodns.error import DNSError
+from aiohttp import ClientResponse, ClientSession
 
 ERROR_MESSAGE_INSECURE_URL = 'An insecure URL was provided while https_only was enabled; refusing to proceed'
 ERROR_MESSAGE_NO_HOSTNAME  = 'The specified URL is missing a hostname; unable to proceed'
-ERROR_MESSAGE_DNS_FAILURE       = 'Failed to resolve the domains IP address; unable to proceed'
+ERROR_MESSAGE_DNS_FAILURE  = 'Failed to resolve the domains IP address; unable to proceed'
 
 
 async def download_file(
         url: str,
-        filepath: str,
-        loop: Optional[asyncio.AbstractEventLoop] = None,
+        fh: typing.Union[io.BufferedIOBase, typing.BinaryIO],
+        loop: typing.Optional[asyncio.AbstractEventLoop] = None,
         ssrf_protection: bool = True,
-        max_size: Optional[int] = None,
+        max_size: typing.Optional[int] = None,
         https_only: bool = False,
-        **session_arts
-):
+        keep_files_open: bool = False,
+        **session_args
+) -> io.BufferedIOBase:
     """
-    Asynchronously downloads a file to the specified filepath
+    Asynchronously downloads a file using the supplied BufferedIOBase
     Args:
         url: The file URL
-        filepath: The filepath to save to. This can be a directory or a full path including the filename and extension.
-            If only a directory is provided, the filename will be whatever the server returns.
+        fh: Any valid BufferedIO instance. Generally either a writable file opened in binary mode, or a BytesIO instance
         loop: An active event loop to use. If not provided, a new one will be created.
         ssrf_protection: Server Side Request Forgery Protection: When enabled, the hostname is resolved prior to making
             an HTTP request to ensure that the provided hostname does not resolve to a private IP address space.
         max_size: The maximum filesize allowed in bytes. If the server returns a content size greater than this limit,
             or tries to send us more content than the server advertises, the download will be rejected.
         https_only: When true, non-secure download requests will be rejected.
-        **session_arts: Any additional kwargs are passed along to aiohttp.ClientSession()
+        keep_files_open: Default behavior is to seek to the beginning and return an open file-handler for BytesIO
+            instances while returning a closed file-handler for everything else. Set this to True if you always want
+            an open file handler returned regardless of the fh type.
+        **session_args: Any additional kwargs are passed along to aiohttp.ClientSession()
 
     Returns:
-
+        io.BufferedIOBase
     """
+    # Make sure we have a valid event loop
+    loop = loop or asyncio.new_event_loop()
+
     # Make sure we have a valid schema
     if not url.lower().startswith(('https://', 'http://')):
         if https_only:
@@ -57,7 +65,7 @@ async def download_file(
 
     # Pre-resolve the hostname if necessary
     if ssrf_protection:
-        resolver = DNSResolver(loop)
+        resolver = DNSResolver(loop=loop)
 
         # We perform DNS resolutions via the systems hosts file first if available
         try:
@@ -66,26 +74,44 @@ async def download_file(
             raise BadUrlError(ERROR_MESSAGE_DNS_FAILURE)
 
         for ip in res.addresses:
-            pass
+            ip = ipaddress.ip_address(ip)  # type: Union[ipaddress.IPv4Address, ipaddress.IPv6Address]
+            if ip.is_private:
+                raise SecurityError(ip)
 
+    # If we're still here, everything should be good. Let's ready the download request.
+    async with ClientSession(loop=loop, raise_for_status=True, **session_args) as session:  # type: ClientSession
+        async with session.get(url, read_until_eof=max_size is None) as response:  # type: ClientResponse
+            if (response.content_length and max_size) and response.content_length > max_size:
+                raise FilesizeError(response.content_length)
 
+            bytes_read = 0
+            async for data in response.content.iter_chunked(1024):
+                # Filesize exceeded. Nuke the file and abort.
+                if max_size and bytes_read > max_size:
+                    fh.close()
+                    if hasattr(fh, 'name'):
+                        os.remove(fh.name)
 
-async def download_bytesio(
-        filepath: str,
-        loop: Optional[asyncio.AbstractEventLoop] = None,
-        ssrf_protection: bool = True,
-        max_size: Optional[int] = None,
-        https_only: bool = False,
-        **session_arts
-):
-    pass
+                    raise FilesizeError(bytes_read)
 
+                fh.write(data)
+                bytes_read += 1024
 
-async def _perform_request():
-    pass
+    if isinstance(fh, io.BytesIO):
+        fh.seek(0)
+        return fh
+
+    if not keep_files_open:
+        fh.close()
+
+    return fh
 
 
 class BadUrlError(Exception):
+    pass
+
+
+class SecurityError(Exception):
     pass
 
 
